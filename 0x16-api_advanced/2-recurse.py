a254b7ae6a5a149755ff2203604a58fcfc7ad330
Vuyo-Lengisi <lvuyo1@gmail.com>
2024-06-05 05:50:14 +0200

    Advanced API

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns a 
+list containing the titles of all hot articles for a 
+given subreddit. If no results are found for the given 
+subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Get all hot post titles"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after:
+        url += "?after={}".format(after)
+    headers = {'User-Agent': 'custom'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()
+    hot_posts = data['data']['children']
+    for post in hot_posts:
+        hot_list.append(post['data']['title'])
+
+    after = data['data']['after']
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
